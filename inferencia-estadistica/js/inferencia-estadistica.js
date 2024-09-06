b92a6f2cba7d9969a0fe187aaa1bd49e43883b26
tonilogar <tologar@gmail.com>
2024-09-06 22:31:25 +0200

    Add files via upload

---
+++ inferencia-estadistica/js/inferencia-estadistica.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - section.clientHeight / 3) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.href.includes(current)) {
+            link.classList.add('active');
+        }
+    });
+});
